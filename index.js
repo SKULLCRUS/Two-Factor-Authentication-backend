856e21ebacbb0dfd62ac2a1310b579414bdfd69f
Rohan <rohanvarshneylko@gmail.com>
2024-01-08 21:24:15 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,108 @@
+const express = require("express");
+const bodyParser = require('body-parser');
+const JsonDB = require('node-json-db').JsonDB;
+const Config = require('node-json-db/dist/lib/JsonDBConfig').Config;
+const uuid = require("uuid");
+const speakeasy = require("speakeasy");
+
+const app = express();
+
+/**
+ * Creates a node-json-db database config
+ * @param {string} name - name of the JSON storage file
+ * @param {boolean} Tells the to save on each push otherwise the save() mthod has to be called.
+ * @param {boolean} Instructs JsonDB to save the database in human readable format
+ * @param {string} separator - the separator to use when accessing database values
+ */
+const dbConfig = new Config("myDataBase", true, false, '/')
+
+/**
+ * Creates a Node-json-db JSON storage file
+ * @param {instance} dbConfig - Node-json-db configuration
+ */
+const db = new JsonDB(dbConfig);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.get("/api", (req,res) => {
+  res.json({ message: "Welcome to the two factor authentication exmaple" })
+});
+
+
+app.post("/api/register", async(req, res) => {
+    const id = uuid.v4();
+    try {
+      const path = `/user/${id}`;
+      // Create temporary secret until it it verified
+      const temp_secret = speakeasy.generateSecret();
+      // Create user in the database
+      await db.push(path, { id, temp_secret });
+      // Send user id and base32 key to user
+      res.json({ id, secret: temp_secret.base32 })
+    } catch(e) {
+      console.log(e);
+      res.status(500).json({ message: 'Error generating secret key'})
+    }
+  })
+
+  app.post("/api/verify", async (req,res) => {
+    const { userId, token } = req.body;
+    try {
+        console.log(userId);
+      // Retrieve user from database
+      const path = `/user/${userId}`;
+      console.log(path);
+      const user =  await db.getData(path);
+      console.log({ user })
+      console.log(user.secret.base32)
+      const  secret  = user.secret.base32;
+      const verified = speakeasy.totp.verify({
+        secret,
+        encoding: 'base32',
+        token
+      });
+      if (verified) {
+        // Update user data
+        let result=await db.push(path, { id: userId, secret: user.secret });
+        console.log(`Updated Result is : ${result}`);
+        res.json({ verified: true })
+      } else {
+        res.json({ verified: false})
+      }
+    } catch(error) {
+      console.error(error);
+      res.status(500).json({ message: 'Error retrieving user'})
+    };
+  })
+
+  app.post("/api/validate", (req,res) => {
+    const { userId, token } = req.body;
+    try {
+      // Retrieve user from database
+      const path = `/user/${userId}`;
+      const user = db.getData(path);
+      console.log({ user })
+      const { base32: secret } = user.secret;
+      // Returns true if the token matches
+      const tokenValidates = speakeasy.totp.verify({
+        secret,
+        encoding: 'base32',
+        token,
+        window: 1
+      });
+      if (tokenValidates) {
+        res.json({ validated: true })
+      } else {
+        res.json({ validated: false})
+      }
+    } catch(error) {
+      console.error(error);
+      res.status(500).json({ message: 'Error retrieving user'})
+    };
+  })
+
+const port = 9000;
+app.listen(port, () => {
+  console.log(`App is running on PORT: ${port}.`);
+});
+
